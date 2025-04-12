65b526418d44a9b4a22fde91c2d5f96483ee1ba6
Your Name <you@example.com>
2025-04-12 19:32:33 -0400

    Updated app with latest changes

--- script.js
+++ script.js
@@ -32,4 +32,35 @@ window.addEventListener('scroll', () => {
   }
 });
 
+let randomNumber = Math.floor(Math.random() * 100) + 1;
+  let attempts = 10;
 
+  function checkGuess() {
+    const inputElement = document.getElementById("guess");
+    const feedbackElement = document.getElementById("feedback");
+    const guess = parseInt(inputElement.value, 10);
+
+    if (guess === randomNumber) {
+      feedbackElement.innerHTML = "Congratulations!";
+      feedbackElement.style.color = "#77a373";
+      attempts = 0;
+    } else {
+      attempts--;
+      if (attempts > 0) {
+        feedbackElement.innerHTML = guess < randomNumber
+          ? "Too low! Try again. " + attempts + " attempts remaining."
+          : "Too high! Try again. " + attempts + " attempts remaining.";
+        feedbackElement.style.color = "#ff404c";
+      } else {
+        feedbackElement.innerHTML = "Sorry, you're out of attempts!";
+        feedbackElement.style.color = "#ff404c";
+      }
+    }
+  }
+
+  function restartGame() {
+    randomNumber = Math.floor(Math.random() * 100) + 1;
+    attempts = 10;
+    document.getElementById("guess").value = '';
+    document.getElementById("feedback").innerHTML = '';
+  }
